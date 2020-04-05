14036a327049ff5f0f4e2b59aa4b12ea58b3fac5
lc <innovgame@163.com>
2020-04-05 10:49:04 +0800

    自定义cube geometry

--- src/chapter6/chapter06.js
+++ src/chapter6/chapter06.js
@@ -226,5 +226,41 @@ var CubeGenerater = (function () {
   return Generater;
 })();
 
+var CustomCubeGeometry = (function () {
+
+  const vertices = [
+    new THREE.Vector3(1, 3, 1),
+    new THREE.Vector3(1, 3, -1),
+    new THREE.Vector3(1, -1, 1),
+    new THREE.Vector3(1, -1, -1),
+    new THREE.Vector3(-1, 3, -1),
+    new THREE.Vector3(-1, 3, 1),
+    new THREE.Vector3(-1, -1, -1),
+    new THREE.Vector3(-1, -1, 1),
+  ];
+
+  const faces = [
+    new THREE.Face3(0, 2, 1),
+    new THREE.Face3(2, 3, 1),
+    new THREE.Face3(4, 6, 5),
+    new THREE.Face3(6, 7, 5),
+    new THREE.Face3(4, 5, 1),
+    new THREE.Face3(5, 0, 1),
+    new THREE.Face3(7, 6, 2),
+    new THREE.Face3(6, 3, 2),
+    new THREE.Face3(5, 7, 0),
+    new THREE.Face3(7, 2, 0),
+    new THREE.Face3(1, 3, 4),
+    new THREE.Face3(3, 6, 4),
+  ];
+
+  const geom = new THREE.Geometry();
+  geom.vertices = vertices;
+  geom.faces = faces;
+  geom.computeFaceNormals();
+
+  return geom;
+})();
+
 const test = new Test();
 test.init();
